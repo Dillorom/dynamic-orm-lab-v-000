ad754208d66dee493faab2efda92eab76beef3d9
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-25 22:38:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,9 +18,9 @@ class InteractiveRecord
   end
 
   )
-    self.column_names.each do |col_name|
+  self.column_names.each do |col_name|
       attr_accessor col_name.to_sym
-    end
+  end
   # def initialize(options = {}
   # end
 
