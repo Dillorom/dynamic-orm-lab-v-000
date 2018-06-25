a4c48b19a2a545401a5ce2e7a064573bfdd6b898
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-25 22:33:57 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,7 +7,7 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hashes = true
+    DB[:conn].results_as_hash = true
     sql = "PRAGMA table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
